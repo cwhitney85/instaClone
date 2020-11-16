0817fd8556bdf72e9276505ac6aec5aa24724d99
Colin Whitney <colin.whitney@gmail.com>
2020-11-16 14:36:24 -0500

    ran CRA and refactored App component to render h1 -CW

---
+++ client_side_instaclone/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>InstaClone</h1>
+      </div>
+    )
+  }
+}
+
