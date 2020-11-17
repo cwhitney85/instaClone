e6e23e3739ed073e9b23315451ba4a3499c5df52
forevermango <raahima95@gmail.com>
2020-11-17 10:43:09 -0600

    user route functioing RA

---
+++ models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+
+//user schema 
+const userSchema = new mongoose.Schema({
+    email: {type: String, required: true},
+    password: {type: String, required: true, minlength: 5},
+    displayName: {type:String}, 
+  })
+  
+module.exports = User = mongoose.model("User", userSchema)
