4a808ea0d0183d8baf36a02f7d2433cffb5cdaed
Colin Whitney <colin.whitney@gmail.com>
2020-11-17 12:19:29 -0500

    deleted userschema from feeds model CW

--- models/feeds.js
+++ models/feeds.js
@@ -9,12 +9,3 @@ const feedSchema = mongoose.Schema({
 })
 
 module.exports = mongoose.model('Feed', feedSchema)
-
-//user schema 
-const userSchema = new mongoose.Schema({
-  email: {type: String, required: true},
-  password: {type: String, required: true, minlength: 5},
-  displayName: {type:String}, 
-})
-
-module.exports = User = mongoose.model("user", userSchema)
