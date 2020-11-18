c852e3a24f611c435fcad4958d5efbc360912c8f
Colin Whitney <colin.whitney@gmail.com>
2020-11-18 16:37:19 -0500

    created get route returning all users as json

--- controllers/user.js
+++ controllers/user.js
@@ -11,6 +11,15 @@ users.get('/testing', (req, res) => {
     res.send('This is the users router now')
 })
 
+users.get('/', (req, res) => {
+    User.find({}, (error, users) => {
+        if (error) {
+            res.status(400).json({ error: error.message })
+        }
+        res.status(200).json(users)
+    })
+})
+
 // Create Route
 users.post('/', async (req, res) => {
     User.create(req.body, (error, user) => {
