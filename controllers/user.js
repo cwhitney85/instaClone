adc00e3d471c30a8e32e57546bb70cba36588a9e
Colin Whitney <colin.whitney@gmail.com>
2020-11-18 16:34:15 -0500

    refactored user controller; set up post route; postman test successful -CW

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,27 @@
-const router = require("express").Router()
+// const router = require("express").Router()
+const express = require('express')
+const users = express.Router()
+const User = require('../models/userModel.js')
 
-router.get('/test',(req, res) => {
-    res.send("Hello, it's working")
+// router.get('/test',(req, res) => {
+//     res.send("Hello, it's working")
+// })
+
+users.get('/testing', (req, res) => {
+    res.send('This is the users router now')
 })
 
+// Create Route
+users.post('/', async (req, res) => {
+    User.create(req.body, (error, user) => {
+        if (error) {
+            res.status(400).json({ error: error.message })
+        }
+        res.status(200).send(user)
+        console.log('new user:', user)
+    })
+})
 
-module.exports = router
+// module.exports = router
+
+module.exports = users
--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,6 @@
+const { ObjectID } = require('bson')
 const mongoose = require('mongoose')
+// const Feed = require('./feeds')
 
 
 //user schema 
@@ -6,6 +8,7 @@ const userSchema = new mongoose.Schema({
     email: {type: String, required: true},
     password: {type: String, required: true, minlength: 5},
     displayName: {type:String}, 
+    // feeds: [{type: ObjectID}]
   })
   
 module.exports = User = mongoose.model("User", userSchema)
