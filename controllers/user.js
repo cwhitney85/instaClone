258a2b97031bc7d6963c47eecb4585bf0f08936c
Colin Whitney <colin.whitney@gmail.com>
2020-11-20 01:43:40 -0500

    login route return a token and user data

--- controllers/user.js
+++ controllers/user.js
@@ -4,7 +4,7 @@ const User = require('../models/userModel.js')
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const auth = require("../middleware/auth");
-// require("dotenv").config()
+require("dotenv").config()
 
 // router.get('/test',(req, res) => {
 //     res.send("Hello, it's working")
@@ -107,35 +107,36 @@ users.post("/register", async (req, res) => {
   }
 });
 
-// users.post("/login", async (req, res) => {
-//   try {
-//     const { email, password } = req.body;
+users.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    console.log(email, password)
+    // validate
+    if (!email || !password)
+      return res.status(400).json({ msg: "Not all fields have been entered." });
 
-//     // validate
-//     if (!email || !password)
-//       return res.status(400).json({ msg: "Not all fields have been entered." });
+    const user = await User.findOne({ email: email });
+    console.log(user)
+    if (!user)
+      return res
+        .status(400)
+        .json({ msg: "No account with this email has been registered." });
 
-//     const user = await User.findOne({ email: email });
-//     if (!user)
-//       return res
-//         .status(400)
-//         .json({ msg: "No account with this email has been registered." });
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials." });
 
-//     const isMatch = await bcrypt.compare(password, user.password);
-//     if (!isMatch) return res.status(400).json({ msg: "Invalid credentials." });
-
-//     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-//     res.json({
-//       token,
-//       user: {
-//         id: user._id,
-//         displayName: user.displayName,
-//       },
-//     });
-//   } catch (err) {
-//     res.status(500).json({ error: err.message });
-//   }
-// });
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    res.json({
+      token,
+      user: {
+        id: user._id,
+        displayName: user.displayName,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 
 
