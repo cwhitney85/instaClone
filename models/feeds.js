fb0e753edddc4aebc0af4db14b3015802c877a77
Raedanwin <Raedanwin@gmail.com>
2020-11-20 14:42:53 -0500

    stupid random commit that i have to do or else it wont let me do anything

--- models/feeds.js
+++ models/feeds.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 
 //feed schema
 const feedSchema = mongoose.Schema({
-  username: {type: String, required: true },
   image: {type: String, required: true},
   title: {type: String, default: 'HappyStagram!'},
   likes: {type: Number, default: 0},
