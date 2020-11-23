d6cf8f885cb10fa560a7d93f58c3ad6632aa2c43
calmworld <sarah.alhuriz@hotmail.com>
2020-11-23 13:35:18 -0700

    Nothing changed

--- models/feeds.js
+++ models/feeds.js
@@ -1,20 +1,12 @@
 const mongoose = require('mongoose')
-//const {ObjectId} = mongoose.Schema.Types
 
-// //comments schema
-// const Comments = mongoose.Schema({
-//   text: {type: String, required: true},
-//   date: {type: Date},
-//   postedBy:{type:ObjectId,ref:"User"}
-// })
 
 //feed schema
 const feedSchema = mongoose.Schema({
   image: {type: String, required: true},
   title: {type: String, default: 'HappyStagram!'},
   likes: {type: Number, default: 0},
-  tags: [{type: String}],
-  // comments: [comments]
+  tags: [{type: String}]
 })
 
 module.exports = mongoose.model('Feed', feedSchema)
