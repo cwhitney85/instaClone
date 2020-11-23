8343ca38b1acab41ea6363cb3d496c51d1d0e7e5
Raedanwin <Raedanwin@gmail.com>
2020-11-23 10:14:54 -0500

    fixed schema

--- models/feeds.js
+++ models/feeds.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose')
 
 //feed schema
 const feedSchema = mongoose.Schema({
+  // displayName: {type: String, required: true},
   image: {type: String, required: true},
   title: {type: String, default: 'HappyStagram!'},
-  likes: {type: Number, default: 0},
-  tags: [{type: String}]
+  likes: [{type:String}],
+  tags: [{type: String}],
+  description: {type: String}
 })
 
 module.exports = mongoose.model('Feed', feedSchema)
